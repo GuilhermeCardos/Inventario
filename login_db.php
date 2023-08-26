16425557dbc1ccba711d56e1f8558800f4a2e135
GuilhermeCardos <luizgcardos@gmail.com>
2023-08-26 13:23:06 -0300

    add files login_bd and logout realese 1.1

---
+++ login_db.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if( empty($_POST) or (empty($_POST["usuario"]) or (empty($_POST["senha"])))){
+        print"<script> location.href='login.php';</script>";
+    }
+
+    include("Database/config.php");
+
+    $usuario= $_POST['usuario'];
+
+    $senha = $_POST['password'];
+
+    $sql = "SELECT * FROM usuario WHERE usuario = '{$usuario}' AND '{$senha}'";
+
+    $res = $conn -> query($sql) or die ($conn->error);
+
+    $row = $res->fetch_object();
+
+    $qtd = $res-> num_rows;
+
+    if($qtd>0){
+        $_SESSION["usuario"]= $usuario;
+        $_SESSION["name"] = $row-> full_name_user;
+      //$_SESSION["tipo"] = $row -> tipo  // TIPOS DE USUARIOS PRIVILÉGIOS - ADMIN | USUÁRIO COMUN  
+
+      print"<script> location.href='index.php';</script>";
+    }else
+    {
+        print"<script> alert ('Usuário e/ou senha incorretos! ');</script>";
+        print"<script> location.href='login.php';</script>";
+    }
